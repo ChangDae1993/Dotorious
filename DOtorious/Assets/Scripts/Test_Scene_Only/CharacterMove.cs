2e7419c8f6659e36430f5f06c7312786b8674084
ChangDae1993 <jsj939639@gmail.com>
2023-09-30 17:46:29 +0900

    TestScene_CharacterMove_ADD

--- DOtorious/Assets/Scripts/Test_Scene_Only/CharacterMove.cs
+++ DOtorious/Assets/Scripts/Test_Scene_Only/CharacterMove.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class CharacterMove : MonoBehaviour
 {
     public float xInput;
+    public float walkSpeed;
+
     [SerializeField] private Rigidbody2D rigid;
     // Start is called before the first frame update
     void Start()
     {
+        walkSpeed = 0.1f;
         rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        xInput = Input.GetAxis("Horizontal");
+        PlayerMove(walkSpeed);
+    }
+
+    public void PlayerMove(float speed)
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetAxisRaw("Horizontal") != 0f)
+        {
+            this.transform.Translate(new Vector2(xInput * speed, 0));
+        }
+        else
+        {
+            speed = 0f;
+            this.transform.Translate(new Vector2(speed, 0));
+        }
     }
 }
