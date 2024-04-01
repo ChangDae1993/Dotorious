f4c8f860bb6b56c94228efa9c1aebfcfc8721759
ChangDae1993 <jsj939639@gmail.com>
2024-04-01 23:45:46 +0900

    Player_RopeAction_Try_02

--- DOtorious/Assets/Test_Scene_Only/RopeAction.cs
+++ DOtorious/Assets/Test_Scene_Only/RopeAction.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class RopeAction : MonoBehaviour
 {
     [Header("Rope Point Detect")]
+    public float pointDetectRange;
     public LayerMask ropePointLayer;
     public Collider2D[] ropePoint;
     //public List<Collider2D> ropePoint = new List<Collider2D>();
-    public float pointDetectRange;
 
-
+    [Space(10f)]
     [Header("Rope Make")]
     public SpringJoint2D joint;
-
+    public Transform ropeShootPoint;
     // Start is called before the first frame update
     void Start()
     {
-
+        joint.anchor = ropeShootPoint.position;
     }
 
     // Update is called once per frame
@@ -29,10 +29,15 @@ public class RopeAction : MonoBehaviour
             if (col.gameObject.TryGetComponent(out RopePoint rP))
             {
                 rP.RopePosition();
-                joint.connectedAnchor = rP.transform.position;
+
+                if (Input.GetKeyDown(KeyCode.LeftControl))
+                {
+                    joint.connectedAnchor = rP.transform.position;
+                }
                 //Debug.Log("Check");
             }
         }
+
     }
 
     private void OnDrawGizmos()
