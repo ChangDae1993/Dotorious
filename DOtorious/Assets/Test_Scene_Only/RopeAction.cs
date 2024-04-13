683d4869b599954a239edcb23c59e6bc0ccd1b19
ChangDae1993 <jsj939639@gmail.com>
2024-04-13 16:30:38 +0900

    Player_RopeAction_Try_05
    overlapCircleAll에서 TriggerEnter2D로 바꾸는 중

--- DOtorious/Assets/Test_Scene_Only/RopeAction.cs
+++ DOtorious/Assets/Test_Scene_Only/RopeAction.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class RopeAction : MonoBehaviour
 {
     [Header("Rope Point Detect")]
-    public float pointDetectRange;
-    public LayerMask ropePointLayer;
-    public Collider2D[] ropePoint;
-    //public List<Collider2D> ropePoint = new List<Collider2D>();
+    //public float pointDetectRange;
+    //public LayerMask ropePointLayer;
+    //public Collider2D[] ropePoint;
+    public List<RopePoint> ropePointList = new List<RopePoint>();
+    public int ropeindex;
 
     [Space(10f)]
     [Header("Rope Make")]
@@ -19,7 +20,7 @@ public class RopeAction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        joint.anchor = ropeShootPoint.position;
+        joint.anchor = ropeShootPoint.localPosition;
         //joint.autoConfigureConnectedAnchor = false;
         //joint.autoConfigureDistance = false;
         //joint.distance = f;
@@ -28,64 +29,103 @@ public class RopeAction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ropePoint = Physics2D.OverlapCircleAll(this.transform.position, pointDetectRange, ropePointLayer);
-        foreach (Collider2D col in ropePoint)
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            if (col.gameObject.TryGetComponent(out RopePoint rP))
+            if (ropeindex < ropePointList.Count - 1)
             {
-                joint.connectedAnchor = rP.transform.position;
-
-                if (Input.GetKey(KeyCode.LeftControl))
-                {
-                    rP.RopePosition();
-
-                    if (Input.GetMouseButtonDown(0))
-                    {
-
-
-                        if (!ropeOnOff)
-                        {
-                            ropeOnOff = true;
-                            Debug.Log("Mouse");
-                            if (!joint.enabled)
-                            {
-                                joint.enabled = true;
-                            }
-                        }
-                        else
-                        {
-                            ropeOnOff = false;
-                            if (joint.enabled)
-                            {
-                                joint.enabled = false;
-                            }
-                        }
-
-                    }
-                }
-                //else
-                //{
-                //    if (joint.enabled)
-                //    {
-                //        joint.enabled = false;
-                //    }
-                //}
-
-                //Debug.Log("Check");
+                ropePointList[ropeindex].RopePosition(true);
+                ropeindex++;
             }
-
-            //if(Input.GetMouseButtonDown(0))
-            //{
-            //    Debug.Log("Mouse");
-            //}
+            else
+            {
+                ropePointList[ropeindex].RopePosition(true);
+                ropeindex = 0;
+            }
         }
 
+        //ropePoint = Physics2D.OverlapCircleAll(this.transform.position, pointDetectRange, ropePointLayer);
+        //foreach (Collider2D col in ropePoint)
+        //{
+        //    if (col.gameObject.TryGetComponent(out RopePoint rP))
+        //    {
+        //        joint.connectedAnchor = rP.transform.position;
+
+        //        if (Input.GetMouseButton(1))
+        //        { 
+        //            rP.pointOn = true;
+        //            if (Input.GetMouseButtonDown(0))
+        //            {
+        //                if (!ropeOnOff)
+        //                {
+        //                    ropeOnOff = true;
+        //                    Debug.Log("Mouse");
+        //                    if (!joint.enabled)
+        //                    {
+        //                        joint.enabled = true;
+        //                    }
+        //                }
+        //                else
+        //                {
+        //                    ropeOnOff = false;
+        //                    if (joint.enabled)
+        //                    {
+        //                        joint.enabled = false;
+        //                    }
+        //                }
+
+        //            }
+        //        }
+        //        else
+        //        {
+        //            rP.pointOn = false;
+        //        }
+        //        //else
+        //        //{
+        //        //    if (joint.enabled)
+        //        //    {
+        //        //        joint.enabled = false;
+        //        //    }
+        //        //}
+
+        //        //Debug.Log("Check");
+        //    }
+
+        //    //if(Input.GetMouseButtonDown(0))
+        //    //{
+        //    //    Debug.Log("Mouse");
+        //    //}
+        //}
+
     }
 
-    private void OnDrawGizmos()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(this.transform.position, pointDetectRange);
+        if (collision.gameObject.TryGetComponent(out RopePoint rp))
+        {
+            ropePointList.Add(rp);
+        }
     }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out RopePoint rp))
+        {
+            if (ropePointList.Count <= 1)
+            {
+                ropeindex = 0;
+            }
+
+            ropePointList[ropeindex].RopePosition(false);
+            ropePointList.Remove(rp);
+            //rp.RopePosition(false);
+        }
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireSphere(this.transform.position, pointDetectRange);
+    //}
 
 }
--- DOtorious/Assets/Test_Scene_Only/RopePoint.cs
+++ DOtorious/Assets/Test_Scene_Only/RopePoint.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class RopePoint : MonoBehaviour
 {
+    public bool pointOn;
+
+    public SpriteRenderer radiance;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //void Update()
+    //{
 
-    public void RopePosition()
+    //}
+
+    public void RopePosition(bool onoFF)
     {
-        Debug.Log("RopePointOn");
+        if (onoFF)
+        {
+            radiance.gameObject.SetActive(true);
+            Debug.Log("RopePointOn");
+        }
+        else
+        {
+            radiance.gameObject.SetActive(false);
+            Debug.Log("RopePointOff");
+        }
     }
 }
